2dd50cc8b4116466c54f076bf595ba9c11830339
yousef-nakhleh <sefnakhleh.ai@gmail.com>
2026-02-07 12:32:18 +0100

    Updated Subtitle.tsx

--- src/sections/home/hero/Subtitle.tsx
+++ src/sections/home/hero/Subtitle.tsx
@@ -1,7 +1,7 @@
 const Subtitle = () => {
   return (
     <p className="text-2xl md:text-4xl font-alex-brush mb-4">
-      Certificazione ufficiale l’Oréal color expert
+      Specialisti in colore e benessere dei capelli, per lei e per lui, nel cuore di Caravaggio
     </p>
   );
 };
